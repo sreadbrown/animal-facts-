4efd1e7723da16ab64b18390e808370fff046494
evan stewart and Sarah Read Brown <sreadbrown@gmail.com>
2016-02-10 19:40:05 -0800

    prompt animals to show up

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $("button#light").click(function() {
-    $("body").removeClass();
-    $("body").addClass("light-background");
+  var animal = prompt("Which animal do you want to learn about? Sloths, Elephants, or Kangaroos?");
+
+  if (animal === 'Sloths') {
+    $('#slothfacts').show();
+  }
+  if (animal === 'Elephants') {
+    $('#elephantfacts').show();
+  }
+  if (animal === "Kangaroos") {
+    $('#kangaroofacts').show();
+  }
 });
